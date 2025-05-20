fbf793351c92d3e11e37d2db9a404ff815357176
Jono Sommers <jonosommers95@gmail.com>
2025-05-20 13:48:32 -0500

    working on happy path tests for GET CARDS endpoint.

---
+++ spec/requests/api/v1/cards_request_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe "Cards API", type: :request do
+    before do
+        create_list(:card, 50)
+    end
+
+    describe "GET /api/v1/cards Happy Paths" do
+        context "when no params are provided" do
+            it "returns the first page of cards with 16 results" do
+                get api_v1_cards_path
+
+                expect(response).to be_successful
+                
+                json = JSON.parse(response.body, symbolize_names: true)
+                first_card = json[:data].first
+
+                expect(json[:data].count).to eq(16)
+                expect(json[:meta]).to include(:current_page, :total_pages, :total_count)
+                expect(json[:meta][:current_page]).to eq(1)
+                expect(first_card).to include(:id, :type, :attributes)
+                expect(first_card[:type]).to eq("card")
+                expect(first_card[:attributes]).to include(
+                    :name,
+                    :set_name,
+                    :image_url,
+                    :supertype,
+                    :subtype,
+                    :rarity,
+                    :types
+                )
+            end
+        end
+
+        context "when requesting a specific page" do
+            it "returns the correct set of cards for that page" do
+            end
+        end
+
+        context "when a name filter is applied" do
+            it "returns cards matching the name" do
+            end
+        end
+
+        context "when a set_name filter is applied" do
+            it "returns the cards from the correct set" do
+            end
+        end
+    end
+
+    describe "GET /api/v1/cards Sad Paths" do
+        context "when no cards match the filter" do
+            it "returns an empty data array with status 200" do
+            end
+        end
+    end
+end
