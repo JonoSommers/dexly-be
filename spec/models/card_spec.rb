aafd6fe8e8d57b6faab8b9d73f6f655a4b9e2361
Jono Sommers <jonosommers95@gmail.com>
2025-05-27 11:38:40 -0500

    test: added validation tests for all models using Shoulda Matchers.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:id) }
+  it { should validate_uniqueness_of(:id) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:set_name) }
+  it { should validate_presence_of(:image_url) }
 end
