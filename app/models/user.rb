075057502bb22654d58cbefc73df72b817f00cdc
Jono Sommers <jonosommers95@gmail.com>
2025-05-27 11:18:20 -0500

    feat: add user creation with password hashing and validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :user_binders, dependent: :destroy
     has_many :binders, through: :user_binders
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 10 }
 end
