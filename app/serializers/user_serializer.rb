7d93cf30db8b42ff523c7554613fa96eb72ab8b0
Jono Sommers <jonosommers95@gmail.com>
2025-05-27 14:52:22 -0500

    feat: included binders in User serializer and update tests accordingly.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,14 @@
 class UserSerializer
     include JSONAPI::Serializer
     attributes :username
+
+    attribute :binders do |user|
+        user.binders.map do |binder|
+            {
+                id: binder.id,
+                name: binder.name,
+                cover_image_url: binder.cover_image_url
+            }
+        end
+    end
 end
