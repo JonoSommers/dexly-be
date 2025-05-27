1a6055d2f800e4fada76b80eef1c46031e2e9a65
Jono Sommers <jonosommers95@gmail.com>
2025-05-27 11:47:33 -0500

    test: fix card uniqueness test with explicit subject setup.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
+  subject { create(:card, id: "sv1-1") }
+  
   it { should validate_presence_of(:id) }
   it { should validate_uniqueness_of(:id) }
 
